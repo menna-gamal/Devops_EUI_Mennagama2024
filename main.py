66e3f6afd44d127322071030832286a327454bf6
mennagamal <menna.g.eid@gmail.com>
2024-06-14 21:37:31 +0300

    Dockerize Flask app and update Dockerfile and docker-compose.yml

--- main.py
+++ main.py
@@ -1,12 +1,39 @@
 from flask import Flask, render_template
+import json
 
 app = Flask(__name__)
 
+counter_file = 'count.json'
+
+
+def read_counter():
+    
+    with open(counter_file, 'r') as file:
+        data = json.load(file)
+        return data.get('hits', 0)
+
+def write_counter(count):
+    with open(counter_file, 'w') as file:
+        json.dump({'hits': count}, file)
 
 @app.route('/')
 def index():
-    return render_template('converter.html')
+    
+   # Read the current count
+    count = read_counter()
+    # Increment the visit count
+    count += 1
+    # Write the new count back to the file
+    write_counter(count)
+    return render_template('converter.html', visit_count=count)
 
+@app.route('/visits')
+def visits():
+
+    # Read the current count
+    count = read_counter()
+    return render_template('visits.html', visit_count=count)
+    
 
 if __name__ == '__main__':
     app.run('0.0.0.0', 5000)
